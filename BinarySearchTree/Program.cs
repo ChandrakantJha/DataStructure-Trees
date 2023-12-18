260f42bb1c6bc991686fd28fde46a0b697f46797
Anjali Shandilya <anjalishandilya52@gmail.com>
2023-12-18 13:32:35 +0530

    Simple Tree structure with Integer values as data

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create Instance of Tree to store integer values
+            Tree<int> tree = new Tree<int>();
+
+            tree.root = new TreeNode<int>() { Data = 100 };
+
+            tree.root.Child = new List<TreeNode<int>>()
+            {
+              new TreeNode<int>() { Data = 50, Parent = tree.root },
+              new TreeNode<int>() { Data = 1, Parent = tree.root },
+              new TreeNode<int>() { Data = 150, Parent = tree.root },
+            };
+
+            tree.root.Child[2].Child = new List<TreeNode<int>>()
+            { new TreeNode<int>() { Data= 30, Parent = tree.root.Child[2] }
+            };
+
+        }
+    }
+}
---
+++ BinarySearchTree/Tree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    /// <summary>
+    /// Represents the whole tree
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Tree<T>
+    {
+        /// <summary>
+        /// property to get access to the root node
+        /// </summary>
+        public TreeNode<T> root { get; set; }
+    }   
+}
